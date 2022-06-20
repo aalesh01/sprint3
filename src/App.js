6bdbfbd7503fe3064a10d96d9f56e2ed72a86152
aalesh01 <aalesh111999@gmail.com>
2022-06-20 22:01:11 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
+import {Route,Routes} from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Login from "./pages/Login"
 
 function App() {
-  return <div className="App">{/* CODE HERE */}</div>;
+  return <div className="App">
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/login" element={<Login />}></Route>
+    </Routes>
+  </div>;
 }
 
 export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  return <div>{/* CODE HERE */}</div>;
+  return (
+    <div>
+      <Link to="/">Home</Link>
+      <Link to="/">Logout</Link>
+    </div>
+  );
 };
 
 export default Navbar;
